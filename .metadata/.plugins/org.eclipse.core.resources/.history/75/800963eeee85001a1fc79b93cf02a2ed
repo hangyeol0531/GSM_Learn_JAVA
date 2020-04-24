a961bd2eb2bb89cc95d8030821d67dd7098e4419
hangyeol0531 <jhan0531@naver.com>
2020-04-24 14:58:48 +0900

    arrylist

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/75/800963eeee85001a1fc79b93cf02a2ed
@@ -0,0 +1,12 @@
+package gsm.util;
+
+public class intArrary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private int[] arr;
+		
+
+	}
+
+}
---
+++ src/gsm/util/IntArray.java
@@ -0,0 +1,15 @@
+package gsm.util;
+
+public class IntArray {
+	
+	private int[] arr;
+	
+	public IntArray() {
+		this(10);
+	}
+
+	
+	public IntArray(int inital) {
+		arr = new int[inital];
+	}
+}
